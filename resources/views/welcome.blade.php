48ecd76cd3b9fc8e1fa4296269e2f249f5f90591
Ary <aryhenriquedaluz@hotmail.com>
2022-12-02 13:17:46 -0300

    Alteracao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Pagina Inicial</title>
         <link rel="stylesheet" href="/css/styles.css">
 
         <!-- Fonts -->
